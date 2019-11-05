a1ae611f4667ce809364f16a66bf7572059a9ded
lukasz <lukasztracz195@gmail.com>
2019-11-05 14:48:27 +0100

    test

--- venv/Alghoritms/mainProgram.py
+++ venv/Alghoritms/mainProgram.py
@@ -1,57 +1,30 @@
 print("isa-car2")
 print("version 0.1")
 
-
 import numpy as np
 import cv2
 
 cap = cv2.VideoCapture(0)
 # cap.open("rtsp://USER:PASS@IP:PORT/Streaming/Channels/2")
 
-while(True):
-     # Capture frame-by-frame
+while (True):
+    # Capture frame-by-frame
     ret, frame = cap.read()
 
     # Our operations on the frame come here
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
+    circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 50, 150, param1=50, param2=51, minRadius=0, maxRadius=200)
+
+    circles = np.uint16(np.around(circles))
+    for i in circles[0, :]:
+        # draw the outer circle
+        cv2.circle(gray, (i[0], i[1]), i[2], (0, 255, 0), 2)
+
+    cv2.imshow('detected circles', gray)
     # Display the resulting frame
-    cv2.imshow('Salida',gray)
-
-
-    image =frame
-    b = image.copy()
-     # set green and red channels to 0
-    b[:, :, 1] = 0
-    b[:, :, 2] = 0
-
-    g = image.copy()
-    # set blue and red channels to 0
-    g[:, :, 0] = 0
-    g[:, :, 2] = 0
-    r = image.copy()
-    # set blue and green channels to 0
-    r[:, :, 0] = 0
-    r[:, :, 1] = 0
-
-     # RGB - Blue
-    cv2.imshow('B-RGB', b)
-
-     # RGB - Green
-    cv2.imshow('G-RGB', g)
-
-     # RGB - Red
-    cv2.imshow('R-RGB', r)
-
-  ##  cv2.waitKey(0)
-
-
-
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-
-
-
 
 # When everything done, release the capture
 cap.release()
