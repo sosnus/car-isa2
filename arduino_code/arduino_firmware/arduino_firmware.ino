840e446a7422cdea8462d2bb6fbac758f6d44bcf
Sosnus <sosnus15@gmail.com>
2019-12-07 04:54:23 +0100

    new func serialToArray()

--- arduino_code/arduino_firmware/arduino_firmware.ino
+++ arduino_code/arduino_firmware/arduino_firmware.ino
@@ -1,18 +1,49 @@
 #include <Servo.h>
 
-uint8_t values[13]= {0,1,2,3,4,5,6,7,8,9,10,11,12};
+uint8_t values[13] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
 
 
-Servo camV; 
-Servo camH; 
-Servo wheels; 
+Servo camV;
+Servo camH;
+Servo wheels;
+
+int intFromSerial = 3;
 
 enum channel {SharpL = 0,  SharpC,  SharpR,  Baterry,  Wheels,  Motor,  Motor2,  Dir1,  Dir2,  CamH,  CamV};
 
 void setup() {
   // put your setup code here, to run once:
-Serial.begin(115200);
-Serial.println("SharpL,  SharpC,  SharpR,  Baterry,  Wheels,  Motor,  Motor2,  Dir1,  Dir2,  CamH,  CamV");
+  Serial.begin(115200);
+  Serial.println("SharpL,  SharpC,  SharpR,  Baterry,  Wheels,  Motor,  Motor2,  Dir1,  Dir2,  CamH,  CamV");
+}
+
+int serialToArray()
+{
+
+  delay(100);
+  Serial.print("New bytes: ");
+  Serial.println(Serial.available());
+  for (int i = 0; i < 12; i++)
+  {
+    intFromSerial = Serial.parseInt();
+    if (i < 3)
+    {
+      if ((i + 256) == intFromSerial)
+        Serial.println("checksum ok!");
+      else
+      {
+        
+        Serial.println("checksum BAD!");
+        return 1;
+      }
+    }
+    else
+    {
+      values[i] = intFromSerial;
+    }
+
+  }
+  return 0;
 }
 
 void loop() {
@@ -22,30 +53,17 @@ void loop() {
   values[SharpR] = analogRead(A2);
   values[Baterry] = analogRead(A3);
 
-  //read from serialport
-  // buffer sync
-  //TODO: buffer sync
-  
-  int intFromSerial = 0;
-  do{
-  intFromSerial = Serial.parseInt();
-  Serial.print("parse test: ");
-  Serial.println(intFromSerial);  
-  }while (intFromSerial != 256);
-  
-  Serial.print("NICE!!!");
-//  values[Motor] = 
+  if (Serial.available())
+  {
+    serialToArray();
+  }
 
-
-  
-  for(uint8_t i = 0; i<12; i++)
+  for (uint8_t i = 0; i < 12; i++)
   {
     Serial.print(values[i]);
     Serial.print('\t');
   }
-    Serial.println(' ');
-  
-  delay(250);
-  // put your main code here, to run repeatedly:
+  Serial.println(' ');
 
+  delay(2500);
 }
