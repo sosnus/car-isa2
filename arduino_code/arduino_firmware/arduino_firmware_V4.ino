245cc9b81020017ed764b47def91d413b27e1a09
Sosnus <sosnus15@gmail.com>
2020-01-14 23:13:15 +0100

    Create arduino_firmware_V4.ino

---
+++ arduino_code/arduino_firmware/arduino_firmware_V4.ino
@@ -0,0 +1,81 @@
+#include <Servo.h>
+
+Servo camera_H;
+Servo camera_V;
+Servo motor_L;
+Servo motor_R;
+
+uint8_t servo205 = 90;
+uint8_t servo206 = 90;
+uint8_t servo209 = 90;
+uint8_t servo210 = 90;
+
+#define cmdCV '0'
+#define cmdCH '1'
+#define cmdML '2'
+#define cmdMR '3'
+
+int receiv = 90;
+
+uint8_t receiv2 = 90;
+
+void setup() {
+  camera_H.attach(5);
+  camera_V.attach(6);
+  motor_L.attach(9);
+  motor_R.attach(10);
+  Serial.begin(9600);
+  Serial.print("\n\rTEST, SETUP() ");
+}
+
+void loop() {
+  delay(1000);
+
+  Serial.print("new: ");
+  Serial.println(Serial.available());
+
+  if ( Serial.available())
+  {
+    receiv = Serial.read();
+    if (receiv >= cmdCV && receiv <= cmdMR)
+    {
+      // NEW COMMAND
+      Serial.print("\n\rReveive: ");
+      Serial.print((char)receiv);
+      Serial.print("=" );
+      delay(50); //wait for new char - value of command
+      receiv2 = Serial.read();
+      receiv2 = (receiv2 - 64) * 5;
+      Serial.println(receiv2);
+
+      if (receiv == cmdCV) // servoDrop
+      {
+        servo205 = receiv2;
+        camera_H.write(servo205);
+        Serial.println("edit CV");
+      }
+      else if (receiv == cmdCH) // servoParachute
+      {
+        servo206 = receiv2;
+        camera_V.write(servo206);
+        Serial.println("edit CH");
+      }
+      else if (receiv == cmdML) // servoParachute
+      {
+        servo209 = receiv2;
+        motor_L.write(servo209);
+        Serial.println("edit ML");
+      }
+      else if (receiv == cmdMR) // servoParachute
+      {
+        servo210 = receiv2;
+        motor_R.write(servo210);
+        Serial.println("edit MR");
+      }
+      else
+      {
+        Serial.println("ERR, bad command");
+      }
+    }
+  }
+}
